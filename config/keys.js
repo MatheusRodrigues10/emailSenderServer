b28b8136dae6937eb31763790d9237de3c797eac
Matheus Rodrigues <matheushehe007@gmail.com>
2025-02-22 17:04:05 -0300

    correção de bug de import do ambiente de dev

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,22 @@
-import devKeys from "./dev.js";
-import prodKeys from "./prod.js";
+//keys.js - verifica ambiente para usar chaves respectivas.
+let keys;
 
-const keys = process.env.NODE_ENV === "production" ? prodKeys : devKeys;
+if (process.env.NODE_ENV === "production") {
+  // Se estiver em produção, use as chaves de produção
+  keys = await import("./prod.js").then((module) => module.default);
+} else {
+  try {
+    // Se estiver em desenvolvimento, tente carregar `dev.js`
+    keys = await import("./dev.js").then((module) => module.default);
+  } catch (error) {
+    console.error("Arquivo dev.js não encontrado! Certifique-se de que ele existe no ambiente de desenvolvimento.");
+  }
+};
+
+/* O .then((module) => module.default) é necessário porque import() em ESM (ECMAScript Modules) retorna um objeto de módulo, e queremos pegar apenas o export default desse módulo.
+    ele retornaria algo como se estivesse sem:
+    const module = await import("./dev.js");
+    console.log(module); === { default: { googleClientID: "...", googleClientSecret: "...", ... } }
+*/
 
 export default keys;
